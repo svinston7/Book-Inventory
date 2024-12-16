215e292801245a1c10a3f157618fccbf6e9cf298
EVITHYA <e-asha.vithya@capgemini.com>
2024-12-16 12:01:55 +0530

    Update ShoppingCartDAO.java
    updatedDAO

--- Backend/src/main/java/com/dao/ShoppingCartDAO.java
+++ Backend/src/main/java/com/dao/ShoppingCartDAO.java
@@ -2,7 +2,7 @@ package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.model.ShoopingCart;
+import com.model.ShoppingCart;
 @Repository
-	public interface AuthorDAO extends JpaRepository<ShoppingCart, String> {
+	public interface AuthorDAO extends JpaRepository<ShoppingCart, Integer> {
 	}
