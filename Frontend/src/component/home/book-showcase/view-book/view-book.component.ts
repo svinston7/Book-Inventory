15f0ad58a59a06ad4319242de2744226b69bee28
svinston7 <sam.vinston-m@capgemini.com>
2024-12-24 10:52:29 +0530

    view book changes

--- Frontend/src/component/home/book-showcase/view-book/view-book.component.ts
+++ Frontend/src/component/home/book-showcase/view-book/view-book.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { ShowAllBooksService } from '../../../../service/show-all-books.service';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from '../../Book';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from "../../home.component";
 
 @Component({
   selector: 'app-view-book',
-  imports: [],
+  imports: [CommonModule, HomeComponent],
   templateUrl: './view-book.component.html',
   styleUrl: './view-book.component.css'
 })
 export class ViewBookComponent {
-
+  isReadonly = true;
   constructor(private service:ShowAllBooksService,private route:ActivatedRoute){}
 
   ngOnInit(){
--- Frontend/src/component/home/home.component.ts
+++ Frontend/src/component/home/home.component.ts
@@ -2,13 +2,22 @@ import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { BookShowcaswComponent } from './book-showcase/book-showcase.component';
+import { Router } from '@angular/router';
+import { ViewBookComponent } from "./book-showcase/view-book/view-book.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule,BookShowcaswComponent],
+  imports: [CommonModule, BookShowcaswComponent, ViewBookComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private router:Router){}
+
+logout() {
+  localStorage.removeItem('token');
+  this.router.navigate(['']); 
+}
 
 }
