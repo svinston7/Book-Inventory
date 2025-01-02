aa1144c8fb3a1238869b635f7b14c80f0b704938
EVITHYA <e-asha.vithya@capgemini.com>
2025-01-02 14:32:03 +0530

    Admin update book component done

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { ProfileComponent } from '../component/profile/profile.component';
 import { AdminComponent } from '../component/admin/admin.component';
 import { AuthGuard } from '../guard/auth.guard';
 import { ShoppingcartComponent } from '../component/shoppingcart/shoppingcart.component';
+import { AdminBookComponent } from '../component/admin/admin-book/admin-book.component';
 
 
 
@@ -26,9 +27,11 @@ export const routes: Routes = [
     {path:'authors',component:AuthorViewComponent,canActivate:[AuthGuard]},
     {path:'bookcondition',component:BookConditionComponent,canActivate:[AuthGuard]},
     {path:'profile',component:ProfileComponent,canActivate:[AuthGuard]},
-    {path:'addbook',component:PublisherComponent,canActivate:[AuthGuard]},  
-    {path:'admin',component:AdminComponent,canActivate:[AuthGuard]},
+    {path:'addbook',component:PublisherComponent,canActivate:[AuthGuard]}, 
+    {path: 'admin', component: AdminComponent, canActivate: [AuthGuard]  
+    },
+   { path: 'admin-book', component: AdminBookComponent, canActivate: [AuthGuard] },
     {path:'shoppingcart',component:ShoppingcartComponent}
-    // { path: '**', redirectTo: '/login' }
+    
   
 ];
