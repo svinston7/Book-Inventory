e18577b5eeeebb96981e184c6a8b3970d3bae68c
Sharmila-200203 <sharmila.murugesan@capgemini.com>
2025-01-03 10:20:21 +0530

    styling change

--- Frontend/src/component/book-showcase/view-book/view-book.component.ts
+++ Frontend/src/component/book-showcase/view-book/view-book.component.ts
@@ -198,7 +198,7 @@ export class ViewBookComponent {
         this.cartservice.postCart(cart).subscribe({
           next: (response) => {console.log(`Cart item added successfully:`, response);
             //window.alert("book added to your cart sucessfully");
-            this.popupMessage = 'Your Book has been sucessfully added to the cart';
+            this.popupMessage = 'Your Book has been sucessfully added to the cart🛒';
         this.isPopupVisible=true;
 
           },
